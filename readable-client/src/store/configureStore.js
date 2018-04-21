20e1794badbe0a4695751b650e24c70aadc7aaa4
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-20 22:15:59 -0400

    feat: redux store configuration

---
+++ readable-client/src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from '../reducers';
+
+const configureStore = initialState => {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, createLogger())),
+  );
+
+  return store;
+};
+export default configureStore;
