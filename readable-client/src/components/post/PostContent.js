a93350e31d779213c4e3f5393b1f9437fa50b628
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-21 17:01:44 -0400

    feat: post content layout change

--- readable-client/src/components/post/PostContent.js
+++ readable-client/src/components/post/PostContent.js
@@ -3,16 +3,19 @@ import CommentCounter from '../comment/CommentCounter';
 
 const PostContent = ({ post }) => (
   <div className="mdl-card__supporting-text">
-    <span className="post-detail-subitem mdl-typography--font-light mdl-typography--body-2">
-      <span className="post-author">{post.author}</span>
-    </span>
-
-    <span className="post-detail-subitem mdl-typography--caption-color-contrast">
-      {post.timestamp}
-    </span>
     <span className="post-detail-subitem mdl-typography--font-light mdl-typography--subhead">
-      <div className="post-contents">{post.body}</div>
+      <div className="post-contents">
+        {post.body} sfsdf sdfsdfsd sdfsdf sdf sdf sdf sdfsdf sdf sdfs sdf sdfsdf
+      </div>
+    </span>
+    <span className="post-detail-subitem mdl-typography--font-light mdl-typography--subhead test">
       <CommentCounter />
+    </span>
+    <span className="post-detail-subitem mdl-typography--caption-color-contrast">
+      Posted On: {new Date(post.timestamp).toLocaleString()}
+    </span>
+    <span className="post-detail-subitem mdl-typography--font-light mdl-typography--body-2">
+      <span className="post-author">By : {post.author}</span>
     </span>
   </div>
 );
