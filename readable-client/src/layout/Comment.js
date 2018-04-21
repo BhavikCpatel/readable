a10bc284adc96fca22af32ca73602962c7c1b2c4
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-20 22:21:12 -0400

    fix: bug fix

--- readable-client/src/layout/Comment.js
+++ readable-client/src/layout/Comment.js
@@ -3,7 +3,7 @@ import VoteCard from './VoteCard';
 import CommentActions from './CommentActions';
 import CommentDetail from './CommentDetail';
 
-const Comment = props => (
+const Comment = () => (
   <li className="mdl-list__item mdl-list__item--three-line comment-detail">
     <CommentDetail />
     <span className="mdl-list__item-secondary-content">
