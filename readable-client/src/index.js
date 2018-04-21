7f285a5560e23bb9e96c589947c590c7cddeb75c
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-20 22:12:58 -0400

    feat: add redux provider

--- readable-client/src/index.js
+++ readable-client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import { Provider } from 'react-redux';
+
+import createStore from './store/configureStore';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  categories: { data: [], isError: false, isLoading: false },
+  posts: { data: [], isError: false, isLoading: false },
+  ui: { orderBy: 'timestamp' },
+};
+const store = createStore(initialState);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
