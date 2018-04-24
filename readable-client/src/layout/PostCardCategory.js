f47434f1f8149e150856ef7b1a1b119c3d50dc3c
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-24 17:47:11 -0400

    refactor: remove layout files which were used for static design

--- readable-client/src/layout/PostCardCategory.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import SectionHeader from './SectionHeader';
-
-const PostCardCategory = props => (
-  <SectionHeader color="grey-600" title={props.category} />
-);
-export default PostCardCategory;
