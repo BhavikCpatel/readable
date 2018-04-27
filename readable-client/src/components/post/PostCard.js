828775db0b07471bf11adcb66b6c378699d914f8
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-27 12:24:40 -0400

    feat: limit number of post body characters for post list page

--- readable-client/src/components/post/PostCard.js
+++ readable-client/src/components/post/PostCard.js
@@ -9,7 +9,7 @@ const Post = ({ post, deletePost, isDetailCard = false, onCommentAdd }) =>
     <React.Fragment>
       <PostCardCategory category={post.category} />
       <PostCardTitle title={post.title} />
-      <PostContent post={post} />
+      <PostContent post={post} isDetailCard={isDetailCard} />
       <PostCardActions
         postId={post.id}
         voteScore={post.voteScore}
--- readable-client/src/components/post/PostContent.js
+++ readable-client/src/components/post/PostContent.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import CommentCounter from './CommentCounter';
 
-const PostContent = ({ post }) => (
-  <div className="mdl-card__supporting-text">
-    <span className="post-detail-subitem mdl-typography--font-light mdl-typography--subhead">
-      <div className="post-contents">{post.body}</div>
-    </span>
-    <span className="post-detail-subitem mdl-typography--font-light mdl-typography--subhead test">
-      <CommentCounter comments={post.commentCount} />
-    </span>
-    <span className="post-detail-subitem mdl-typography--caption-color-contrast">
-      Posted On: {new Date(post.timestamp).toLocaleString()}
-    </span>
-    <span className="post-detail-subitem mdl-typography--font-light mdl-typography--body-2">
-      <span className="post-author">By : {post.author}</span>
-    </span>
-  </div>
-);
+const PostContent = ({ post, isDetailCard }) => {
+  const postBody = isDetailCard ? post.body : post.body.slice(0, 50);
+  return (
+    <div className="mdl-card__supporting-text">
+      <span className="post-detail-subitem mdl-typography--font-light mdl-typography--subhead">
+        <div className="post-contents">
+          {postBody}
+          {post.body !== postBody ? '...' : ''}
+        </div>
+      </span>
+      <span className="post-detail-subitem mdl-typography--font-light mdl-typography--subhead test">
+        <CommentCounter comments={post.commentCount} />
+      </span>
+      <span className="post-detail-subitem mdl-typography--caption-color-contrast">
+        Posted On: {new Date(post.timestamp).toLocaleString()}
+      </span>
+      <span className="post-detail-subitem mdl-typography--font-light mdl-typography--body-2">
+        <span className="post-author">By : {post.author}</span>
+      </span>
+    </div>
+  );
+};
 
 export default PostContent;
