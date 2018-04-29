20c0c0242241231c0ff2ee3ddaa379f9c3e983c4
Bhavik Patel <bhavikcpatel@gmail.com>
2018-04-29 16:51:42 -0400

    doc: add comment

--- readable-client/src/index.js
+++ readable-client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-
 import createStore from './store/configureStore';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './styles/index.css';
 
+/* set initial state for app */
 const initialState = {
   categories: { data: [], isError: false, isLoading: false },
   posts: { data: [], isError: false, isLoading: false },
@@ -15,11 +15,16 @@ const initialState = {
     notification: null,
   },
 };
+
+/* create store */
 const store = createStore(initialState);
+
+/* Render app */
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root'),
 );
+
 registerServiceWorker();
